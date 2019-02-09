8267f8fcbc1c15ecb7f46870f18d6d1ad2a49899
Schnibel <schnibel@gmail.com>
2019-02-09 16:56:15 +0100

    Update index.js

--- bindings/nodejs/index.js
+++ bindings/nodejs/index.js
@@ -1,12 +1,12 @@
 //index.js
 const nodejs_binding = require('./build/Release/rpi_rgb_led_matrix_nodejs.node');
 
-console.log("Let's go....");
-console.log('addon',nodejs_binding);
-console.log('say Hello = ', nodejs_binding.hello());
-console.log("add 5 to 6 = ", nodejs_binding.add(5, 6));
+console.log("1 - Let's go....");
+//console.log('addon',nodejs_binding);
+//console.log('say Hello = ', nodejs_binding.hello());
+//console.log("add 5 to 6 = ", nodejs_binding.add(5, 6));
 
-const classInstance = new nodejs_binding.ClassExample(4.3);
+const classInstance = new nodejs_binding.ActualClass(4.3);
 console.log('Testing class initial value : ',classInstance.getValue());
 console.log('After adding 3.3 : ',classInstance.add(3.3));
 module.exports = nodejs_binding;
